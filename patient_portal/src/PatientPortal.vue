22fae2f89ca066eeb8d32f414e1069edb579dd21
Sajin S R <sajin@earthianslive.com>
2025-09-08 17:41:56 +0530

    fix(Patient Portal): convert appointment list as a component

--- patient_portal/src/PatientPortal.vue
+++ patient_portal/src/PatientPortal.vue
@@ -4,77 +4,7 @@
 			<Tabs as="div" v-model="portal_tabs" :tabs="tabs">
 				<template #tab-panel="{ tab }">
 					<div v-if="tab.label == 'Appointments'">
-						<div class="flex justify-end m-2">
-							<Button
-								variant="solid"
-								:label="'Book'"
-								@click="make_appointment_dialog = true"
-							>
-								<template #prefix>
-									<FeatherIcon name="plus" class="h-4" />
-								</template>
-							</Button>
-						</div>
-						<div class="py-4 relative min-h-[75vh] flex flex-col">
-							<!-- Appointment Grid -->
-							<div
-								v-if="paginatedAppointments.length"
-								class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-2
-									max-h-[75vh] overflow-y-auto lg:max-h-none lg:overflow-visible">
-								<Card
-									v-for="item in paginatedAppointments"
-									:key="item.name"
-									class="cursor-pointer rounded-xl border border-gray-200 transition-transform
-										hover:scale-105 duration-200 hover:drop-shadow-md p-4 bg-white
-										min-w-0 !shadow-none drop-shadow-xl"
-									@click="appointmentDetails(item)">
-									<div class="flex items-center justify-between whitespace-nowrap">
-										<h3 class="text-xs font-medium text-gray-500 truncate">{{ item.name }}</h3>
-										<Badge :variant="'outline'"
-											:theme="['Confirmed', 'Closed', 'Checked In'].includes(item.status) ? 'green' : 'orange'">
-											{{ item.status }}
-										</Badge>
-									</div>
-
-									<p class="mt-2 text-md font-semibold text-gray-800 truncate">
-										{{ item.title }}
-									</p>
-
-									<p class="mt-1 text-xs text-gray-600 whitespace-nowrap">
-										<FeatherIcon name="calendar" class="inline w-3 h-3 mr-1 text-gray-500" />
-										{{ formatDate(item.appointment_date) }}
-									</p>
-									<p class="mt-1 text-xs text-gray-600 whitespace-nowrap">
-										<FeatherIcon name="clock" class="inline w-3 h-3 mr-1 text-gray-500" />
-										{{ item.appointment_time }} ({{ item.duration }} mins)
-									</p>
-								</Card>
-							</div>
-
-							<div
-								v-else
-								class="flex flex-col items-center justify-center flex-grow text-center p-6"
-							>
-								<FeatherIcon name="file-text" class="w-12 h-12 text-gray-400 mb-3" />
-								<h2 class="text-lg font-semibold text-gray-700">No Records Found</h2>
-								<p class="text-sm text-gray-500">Looks like you don't have any appointments yet.</p>
-							</div>
-
-							<!-- Pagination -->
-							<div v-if="paginatedAppointments.length" class="flex justify-center items-center space-x-2 mt-auto pt-2">
-								<Button variant="subtle" :disabled="currentPage === 1" @click="currentPage--">
-									Prev
-								</Button>
-
-								<span class="text-sm text-gray-600">
-									Page {{ currentPage }} of {{ totalPages }}
-								</span>
-
-								<Button variant="subtle" :disabled="currentPage === totalPages" @click="currentPage++">
-									Next
-								</Button>
-							</div>
-						</div>
+						<AppointmentModel />
 					</div>
 					<div v-else-if="tab.label == 'Diagnostics'">
 						<DiagnosticModel />
@@ -84,99 +14,6 @@
 		</div>
 	</div>
 
-	<BookAppointmentModel
-		v-if="make_appointment_dialog"
-		v-model="make_appointment_dialog"
-		@appointment_booked="get_appointments.reload()"
-	/>
-
-	<Dialog v-model="appointment_details" :options="{
-		size: '4xl',
-	}">
-		<template #body-title>
-			<div>
-				<h2 class="text-xl font-semibold text-gray-900">Appointment Details</h2>
-			</div>
-			<div class="py-2 flex items-center justify-between gap-2">
-				<p class="text-sm text-gray-500">#{{ selectedAppointment.name }}</p>
-				<Badge :variant="'outline'" size="sm"
-					:theme="['Confirmed', 'Closed', 'Checked In'].includes(selectedAppointment.status) ? 'green' : 'orange'">
-					{{ selectedAppointment.status }}
-				</Badge>
-			</div>
-		</template>
-		<template #body-content>
-			<div class="p-2">
-				<div class="rounded-xl shadow-sm p-4 bg-gray-50 space-y-6">
-					<div class="flex items-start gap-6">
-						<img v-if="selectedAppointment.practitioner_image" :src="selectedAppointment.practitioner_image"
-							class="w-20 h-20 rounded-full object-cover border" />
-						<div v-else
-							class="w-20 h-20 rounded-full object-cover border flex items-center justify-center text-gray-700 text-2xl font-semibold">
-							{{ selectedAppointment.practitioner_name.charAt(0).toUpperCase() }}
-						</div>
-
-						<div class="grid grid-cols-2 gap-6 flex-1">
-							<div>
-								<h3 class="text-gray-700 font-medium">Practitioner</h3>
-								<p class="mt-1 text-lg font-semibold text-gray-900">{{
-									selectedAppointment.practitioner_name }}</p>
-								<p class="mt-1 text-sm text-gray-600">{{ selectedAppointment.department }}</p>
-								<div class="mt-4">
-									<h3 class="text-gray-700 font-medium">When</h3>
-									<p class="mt-1 text-lg font-semibold text-gray-900">
-										{{ formatDate(selectedAppointment.appointment_date) }}
-									</p>
-									<p class="mt-1 text-sm text-gray-600">
-										{{ selectedAppointment.appointment_time }} ({{ selectedAppointment.duration }} mins)
-									</p>
-								</div>
-								<div class="mt-4" v-if="selectedAppointment.encounter">
-									<h3 class="text-gray-700 font-medium">Encounter ID</h3>
-									<p class="mt-1 text-lg font-semibold text-gray-900">{{ selectedAppointment.encounter
-										}}</p>
-								</div>
-							</div>
-
-							<div>
-								<h3 class="text-gray-700 font-medium">Patient</h3>
-								<p class="mt-1 text-lg font-semibold text-gray-900">
-									{{ selectedAppointment.patient_name }}
-								</p>
-								<p class="mt-1 text-sm text-gray-600">{{ selectedAppointment.patient_email }}</p>
-								<p class="mt-1 text-sm text-gray-600">Sex: {{ selectedAppointment.patient_sex }}</p>
-								<div class="mt-4">
-									<div class="flex items-center justify-between">
-										<h3 class="text-gray-700 font-medium">Fee</h3>
-										<Badge :variant="'outline'"
-											:theme="selectedAppointment.invoiced == 1 ? 'green' : 'red'">
-											{{ selectedAppointment.invoiced ? 'Paid' : 'Unpaid' }}
-										</Badge>
-									</div>
-									<p class="mt-1 text-lg font-semibold text-gray-900">
-										{{ formatCurrency(selectedAppointment.paid_amount, selectedAppointment.default_currency) }}
-									</p>
-									<p class="mt-1 text-sm text-gray-600">{{ selectedAppointment.billing_item }}</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</template>
-		<template #actions>
-			<div class="flex justify-center gap-2" v-if="selectedAppointment.encounter">
-				<Button :ref_for="true" theme="gray" size="md" label="print_encounter"
-					@click="print('Patient Encounter', selectedAppointment.encounter)">
-					<Tooltip :text="'Print Prescription'" placement="top">
-						<slot name="icon">
-							<FeatherIcon :name="'printer'" class="size-4 text-ink-white-7" />
-						</slot>
-					</Tooltip>
-				</Button>
-			</div>
-		</template>
-	</Dialog>
 
 	<Dialog :options="{
 		title: dialog_title,
@@ -197,30 +34,19 @@
 
 <script setup>
 import { ref, computed } from 'vue'
-import BookAppointmentModel from '@/components/BookAppointmentModel.vue'
+import AppointmentModel from '@/components/AppointmentModel.vue'
 import DiagnosticModel from '@/components/DiagnosticModel.vue'
-import { formatCurrency } from "@/utils/formatters"
 
 import {
 	createResource,
-	Button,
 	Tabs,
 	Dialog,
-	Badge,
-	Tooltip
 } from 'frappe-ui'
 
-let appointment_details = ref(false);
-let make_appointment_dialog = ref(false);
 let alert_dialog = ref(false);
 
-let appointments = ref([]);
-
 const portal_tabs = ref(0);
-const currentPage = ref(1);
-const pageSize = 16;
 
-const selectedAppointment = ref("");
 let dialog_title = ref("");
 let dialog_message = ref("");
 
@@ -245,22 +71,6 @@ const tabs = computed(() => {
 	return baseTabs
 })
 
-let get_appointments = createResource({
-	url: "/api/method/healthcare.healthcare.api.patient_portal.get_appointments",
-	method: "GET",
-	onSuccess(response) {
-		if (response) {
-			appointments.value = response;
-		}
-	},
-	onError(error) {
-		dialog_message = error.messages?.[0] || error;
-		dialog_title = "Failed to load appointments";
-		alert_dialog.value = true;
-	}
-});
-get_appointments.fetch();
-
 let set_logged_in_patient = createResource({
 	url: "/api/method/healthcare.healthcare.api.patient_portal.get_logged_in_patient",
 	method: "GET",
@@ -277,78 +87,4 @@ let set_logged_in_patient = createResource({
 });
 set_logged_in_patient.fetch();
 
-function appointmentDetails(appointment) {
-	selectedAppointment.value = appointment;
-	appointment_details.value = true;
-}
-
-function print(doctype, docname) {
-	let get_print_format = createResource({
-		url: "/api/method/healthcare.healthcare.api.patient_portal.get_print_format",
-		method: "POST",
-		makeParams() {
-			return {
-				doctype: doctype,
-				name: docname
-			}
-		},
-		onSuccess(response) {
-			if (response) {
-				const with_letterhead = response.letter_head ? 1 : 0;
-				const print_format = response.print_format;
-				const doc_names = JSON.stringify([docname,]);
-				const letterhead = response.letter_head;
-
-				let pdf_options = JSON.stringify({
-					"page-size": "A4",
-					"margin-top": "60mm",
-					"margin-bottom": "60mm",
-					"margin-left": "5mm",
-					"margin-right": "5mm",
-				});
-
-	const w = window.open(
-					"/api/method/frappe.utils.print_format.download_multi_pdf?" +
-						"doctype=" +
-						encodeURIComponent(doctype) +
-						"&name=" +
-						encodeURIComponent(doc_names) +
-						"&format=" +
-						encodeURIComponent(print_format) +
-						"&no_letterhead=" +
-						(with_letterhead ? "0" : "1") +
-						"&letterhead=" +
-						encodeURIComponent(letterhead) +
-						"&options=" +
-						encodeURIComponent(pdf_options)
-	);
-
-	if (!w) {
-					alert("Please enable pop-ups");
-		return;
-	}
-			}
-		}
-	});
-	get_print_format.fetch();
-}
-
-function formatDate(dateStr) {
-	return new Date(dateStr).toLocaleDateString("en-IN", {
-		weekday: "long",
-		year: "numeric",
-		month: "long",
-		day: "numeric"
-	});
-}
-
-// Pagination logic
-const totalPages = computed(() =>
-	Math.ceil(appointments.value.length / pageSize)
-);
-
-const paginatedAppointments = computed(() => {
-	const start = (currentPage.value - 1) * pageSize;
-	return appointments.value.slice(start, start + pageSize);
-});
 </script>
